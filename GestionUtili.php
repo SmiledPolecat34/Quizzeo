f271e16416d8d7496a0ceb73111a3e50911b2a76
sossoIpssi <sophie.tondeur@yahoo.com>
2023-02-16 10:21:29 +0100

    Page de gestion des utilisateurs

---
+++ GestionUtili.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' type='text/css' media='screen' href='GestionUtili.css'>
+    <title>QUIZZEO_Gestion_Utilisateur</title>
+</head>
+
+<body>
+    <header class="title">
+            <h1>Quizzeo</h1>
+    </header>
+    <center><h1>Gestion des Utilisateurs</h1></center>
+    <p>Utilisateurs :</p>
+</body>
