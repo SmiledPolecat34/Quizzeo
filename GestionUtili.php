17f10e09025a0391add0ac353086f3d48bcb15e1
sossoIpssi <sophie.tondeur@yahoo.com>
2023-02-17 11:47:23 +0100

    Page de gestion des utilisateurs

--- GestionUtili.php
+++ GestionUtili.php
@@ -2,13 +2,13 @@
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     $mysqli = new mysqli("localhost", "root", "", "quizzeo");
 
-    $id_Utili= 3;
+    // $id_Utili= 3;
     // $id_Utili=$_POST['Id_utilisateur'];
     function SupprimerUtili($id_Utili){
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         $mysqli = new mysqli("localhost", "root", "", "quizzeo");
-        // $mysqli->query("DELETE FROM `quizzeo`.`utilisateur` WHERE (`Id_utilisateur` = '$id_Utili');");
-        $mysqli->prepare("DELETE FROM `quizzeo`.`utilisateur` WHERE $id_Utili = ?")->execute(array($id_Utili));
+        $mysqli->query("DELETE FROM `quizzeo`.`utilisateur` WHERE (`Id_utilisateur` = '$id_Utili');");
+        // $mysqli->prepare("DELETE FROM `quizzeo`.`utilisateur` WHERE $id_Utili = ?")->execute(array($id_Utili));
         header("Refresh:0");
         echo "L'utilisateur a été supprimé";
         // $redirect = "/";
@@ -37,26 +37,43 @@
         <?php
             mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
             $mysqli = new mysqli("localhost", "root", "", "quizzeo");
-            
+
             function AfficherProfil(){
                 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
                 $mysqli = new mysqli ("localhost", "root", "", "quizzeo");
                 $Profil = $mysqli->query("SELECT * FROM `quizzeo`.`utilisateur`;");
                 if (mysqli_num_rows($Profil) > 0){
                     while ($ligne = mysqli_fetch_assoc($Profil)){
-                        echo "- id : " . $ligne["Id_utilisateur"]."- nom : " . $ligne["pseudutilisateuro"]." --- Email : ".$ligne["email"]." --- MDP : ".$ligne["motDePasse"]."<input type='submit' value='Modifier' name='Modifier'>"."<input type='submit' value='Supprimer' name='Supprimer'>"."<br>";
+                        echo "- id : " . $ligne["Id_utilisateur"]."- nom : " . $ligne["pseudutilisateuro"]." --- Email : ".$ligne["email"]." --- MDP : ".$ligne["motDePasse"]."<br>";
                     }
                 }else{
                     echo "0 resultats";
                 }
             }
             AfficherProfil();
-        
+
             if(isset($_POST["Supprimer"])){
                 SupprimerUtili($id_Utili);
             }
         ?>
     </h2>
+    <div>
+    <div id="modifier">
+            <label for="modif">
+                Quel utilisateur voulez-vous modifier ?
+            </label>
+            <input id="modif1" type="text" name="modif1" placeholder="Id de l'utilisateur" required=""/>
+        </div>
+        <input type='submit' value='Modifier' name='Modifier'>
+        
+        <div id="supprimer">
+            <label for="supp">
+                Quel utilisateur voulez-vous supprimer ?
+            </label>
+            <input id="supp1" type="text" name="supp1" placeholder="Id de l'utilisateur" required=""/>
+        </div>
+        <input type='submit' name='submit' value='Supprimer' class="submitsupprimer">
+    </div>
     <div class="bouton">
         <div class="accueil">
             <a href="page.php">Accueil</a>
