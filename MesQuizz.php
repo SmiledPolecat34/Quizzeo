5f57d7143e2019ec01507884e90265195a2de3f6
sossoIpssi <sophie.tondeur@yahoo.com>
2023-03-14 15:53:17 +0100

    Les quizz de l'utilisateur (modification)

--- MesQuizz.php
+++ MesQuizz.php
@@ -11,7 +11,7 @@
     <header class="title">
         <h1>Quizzeo</h1>
     </header>
-    <form action="Question.php" method="post">
+    <form action="modificationQuizz.php" method="post">
         <?php
             session_start();
             $pseudoco=$_SESSION['pseudo'];
@@ -23,27 +23,44 @@
                     $user = mysqli_fetch_array($idUti);
                     $id_utilisateur = $user["Id_utilisateur"];
                     $Quizz=$mysqli->query("SELECT * FROM `quizzeo`.`quizz` where Id_utilisateur='$id_utilisateur'");
-                    if (mysqli_num_rows($Quizz) > 0){
+                    if (mysqli_num_rows($Quizz) > 0) {
                         // Affichage des données de chaque ligne
-                        while ($ligne = mysqli_fetch_assoc($Quizz)){
-                            echo " Nom : " . $ligne["titre"]. " Catégorie : " . $ligne["categorie"]. " Date de création : " . $ligne["date_creation"].""."\r\n";
-                            ?>
-                                <input type="submit" name="debut_quizz" value="Modifier le Quizz" class="submitconnexion">
-                                <br>
+                        while ($ligne = mysqli_fetch_assoc($Quizz)) {
+                            echo " Nom : " . $ligne["Id_quizz"]." Titre : " . $ligne["titre"]. " Catégorie : " . $ligne["categorie"]. " Date de création : " . $ligne["date_creation"].""."\r\n";
+                            ?>                                <br>
                             <?php 
                         }
-                    }else {
+                    } else {
                         echo "Vous n'avez pas encore créé de quizz";
                     }
                 }
-            
             AffichageQuizzQuizzeur ($pseudoco,$mdpco);
             ?>
-    <div class="bouton">
-        <div class="accueil">
+            <div id="Crea" >
+            <label for="nom_Quizz">
+                Entrez un nom pour le Quizz:
+            </label>
+            <input id="nom_Quizz" class="case" type="text" name="nom_Quizz" placeholder="Nom du quizz"/>
+            </div>
+            <input type="submit" name="modif_quizz" value="Modifier le quizz" class="submitconnexion">
+            <br>
+            <div class="bouton">
+            <div class="accueil">
             <a href="accueil.php">Accueil</a>
+            </div>
         </div>
-    </div>  
+        <?php
+        //When the button is clicked
+        if(isset($_POST["modif_quizz"])){
+            //Retrieves the name of the quiz and will select it in the database
+            $idQuizz=$_POST['nom_Quizz'];
+            $_SESSION['idarecup']=$idQuizz;
+            $mysqli = new mysqli("localhost", "root", "", "quizzeo");
+            $resultat=$mysqli->query("SELECT * FROM `quizzeo`.`quizz` where Id_quizz=$idQuizz");
+            header("Location: http://localhost/Quizzeo/ModificationQuizz.php");
+
+        }
+        ?>  
         <!-- <input type="submit" name="suivant2" value="Suivant" class="submit-suivant"> -->
     </form>
 </body>
