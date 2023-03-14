a3cecbf32745ceaba6c9bcafc706390938c0cafb
sossoIpssi <sophie.tondeur@yahoo.com>
2023-03-14 13:34:35 +0100

    Affichage question

--- QuestionsTest.php
+++ QuestionsTest.php
@@ -12,14 +12,14 @@
     <div class="quiz-container" id ="quiz">
         <div class="quiz-header">
         <form action="QuestionsTest.php" method="post">
-            <h2 id="question" >Question Text</h2>
             <?php
             session_start();
-                $idQuizz=4;
+                $idQuizz=$_SESSION['idarecup'];
                 $mysqli = new mysqli("localhost", "root", "", "quizzeo");
-                $Quizz=$mysqli->query("SELECT * FROM `quizzeo`.`question` WHERE Id_quizz='$idQuizz';");
+                $Quizz=$mysqli->query("SELECT * FROM `quizzeo`.`question` WHERE Id_quizz='$idQuizz';");                
                 if($_SESSION['i']==null){
                     $i=0;
+                    $score=0;
                 }else{
                     $i=$_SESSION['i'];
                 }
@@ -27,9 +27,12 @@
                 while($ligne=mysqli_fetch_array($Quizz)){
                     $d[]=$ligne['intituleQuestion'];
                 }
+                $u=$i+1;
+
                 $nbrQuestion=count($d);
+                
                 if($i<$nbrQuestion){
-                        echo "<div id='variable a passer'>"."$i : ".$d[$i]."\r\n"."</div>";
+                    echo "<h2 id='question'>"."$u : ".$d[$i]."\r\n"."</h2>";    
                 }
 
                 //Displaying data for each row
@@ -39,12 +42,9 @@
                 //         $i++;
                 //     }}
             ?>
-            <ul>
-                <li>
-                    <input type="radio" name="a" id="a" class="reponse">
-                    <label for="a" name ="atext" id="a_text">Réponse a</label>
+            
+                    
                     <?php
-                    $idQuizz=4;
                     //Display of choice a thanks to the database
                     $mysqli = new mysqli("localhost", "root", "", "quizzeo");
                     $Quizz=$mysqli->query("SELECT * FROM `quizzeo`.`question` WHERE Id_quizz='$idQuizz';");
@@ -54,20 +54,23 @@
                     }
                     $nbrQuestion=count($d);
                     if($i<$nbrQuestion){
-                            echo "<div id='variable a passer'>"."$i : ".$e[$i]."\r\n"."</div>";
+                        echo "<ul>";
+                        echo"<li>";
+                        echo"<input type='radio' name='choix' id='a' value='$e[$i]' class='reponse' required>";
+                        echo"<label for='a' name ='atext' id='a_text'>$e[$i]</label>";
+                        echo"</li>";
                     }
                     // if (mysqli_num_rows($Quizz) > 0) {
                     //   while ($ligne = mysqli_fetch_array($Quizz)) {
                     //       echo "<div id='variable a passer'>".$ligne["choix_1"]."\r\n"."</div>";
                     //   }}
+                    
                       
                 ?>
-                </li>
+                
                 <li>
-                    <input type="radio" name="b" id="b" class="reponse">
-                    <label for="b" name ="btext" id="btext">Réponse b</label>
+                    
                     <?php
-                    $idQuizz=4;
                     //Display of choice 2 thanks to the database
                     $mysqli = new mysqli("localhost", "root", "", "quizzeo");
                     $Quizz=$mysqli->query("SELECT * FROM `quizzeo`.`question` WHERE Id_quizz='$idQuizz';");
@@ -77,43 +80,33 @@
                     }
                     $nbrQuestion=count($d);
                     if($i<$nbrQuestion){
-                            echo "<div id='variable a passer'>"."$i : ".$f[$i]."\r\n"."</div>";
+                            echo "<input type='radio' name='choix' id='b' value='$f[$i]' class='reponse'required>";
+                            echo "<label for='b' name ='b_text' id='btext'>$f[$i]</label>";
                     }
-                    // if (mysqli_num_rows($Quizz) > 0) {
-                    //     while ($ligne = mysqli_fetch_assoc($Quizz)) {
-                    //         echo "<div id='variable a passer'>".$ligne["choix_2"]."\r\n"."</div>";
-                    //     }}
+
                     ?>
                 </li>
                 <li>
-                    <input type="radio" name="c" id="c" class="reponse">
-                    <label for="c" id="c_text">Réponse c</label>
 
                 </li>
                 <?php
-                    $idQuizz=4;
                     //Display of choice 3 thanks to the database
                     $mysqli = new mysqli("localhost", "root", "", "quizzeo");
                     $Quizz=$mysqli->query("SELECT * FROM `quizzeo`.`question` WHERE Id_quizz='$idQuizz';");
                     $f=array();
                     while($ligne=mysqli_fetch_array($Quizz)){
-                        $f[]=$ligne['choix_3'];
+                        $h[]=$ligne['choix_3'];
                     }
-                    $nbrQuestion=count($f);
+                    $nbrQuestion=count($d);
                     if($i<$nbrQuestion){
-                            echo "<div id='variable a passer'>"."$i : ".$f[$i]."\r\n"."</div>";
+                            echo "<input type='radio' value='$h[$i]' name='choix' id='c' class='reponse' required >";
+                            echo "<label for='c' id='c_text'>$h[$i]</label>";
                     }
-                    // if (mysqli_num_rows($Quizz) > 0) {
-                    //     while ($ligne = mysqli_fetch_assoc($Quizz)) {
-                    //         echo "<div id='variable a passer'>".$ligne["choix_3"]."\r\n"."</div>";
-                    //     }}
+
                 ?>
                 <li>
-                    <input type="radio" name="d" id="d" class="reponse">
-                    <label for="d" id="d_text">Réponse d</label>
                     <?php
                                  
-                $idQuizz=4;
                 //Display of choice 4 thanks to the database
                   $mysqli = new mysqli("localhost", "root", "", "quizzeo");
                   $Quizz=$mysqli->query("SELECT * FROM `quizzeo`.`question` WHERE Id_quizz='$idQuizz';");
@@ -121,21 +114,38 @@
                   while($ligne=mysqli_fetch_array($Quizz)){
                       $g[]=$ligne['choix_4'];
                   }
-                  $nbrQuestion=count($g);
+                  $nbrQuestion=count($d);
                   if($i<$nbrQuestion){
-                          echo "<div id='variable a passer'>"."$i : ".$g[$i]."\r\n"."</div>";
+                    echo"<input type='radio' name='choix' value='$g[$i]' id='d' class='reponse' required>";
+                    echo"<label for='d' id='d_text'>$g[$i]</label>";
                   }
-                //   if (mysqli_num_rows($Quizz) > 0) {
-                //     while ($ligne = mysqli_fetch_assoc($Quizz)) {
-                //         echo "<div id='variable a passer'>".$ligne["choix_4"]."\r\n"."</div>";
-                //     }}
+
                 if(isset($_POST["Submit"])){
-                    $i++;
-                    $_SESSION['i']=$i;
+                    if(isset($_POST["choix"])){
+                        $reponseUti=$_POST["choix"];
+                        $mysqli = new mysqli("localhost", "root", "", "quizzeo");
+                        $Quizz=$mysqli->query("SELECT * FROM `quizzeo`.`question` WHERE Id_quizz='$idQuizz';");
+                        $k=array();
+                        while($ligne=mysqli_fetch_array($Quizz)){
+                            $k[]=$ligne['reponse'];
+                            
+                        }
+                        if($reponseUti==$k[$i]){
+                            $score=$score+1;
+                        }
+                        $i++;
+                        $_SESSION['i']=$i;
+                    }
+
                     if($i>$nbrQuestion-1){
-                        $_SESSION['i']=null;
+                        $_SESSION['nbrquest']=$nbrQuestion;
+                        $_SESSION['scores']=$score;
+                  
                         header("Location: http://localhost/Quizzeo/score.php");
+                        $_SESSION['i']=null;
+
                     }else{
+                        $_SESSION['scores']=$score;
                         header("Location: http://localhost/Quizzeo/QuestionsTest.php");
                     }
                 }
@@ -145,13 +155,15 @@
 
                 
             </ul>
+            
         </div>
 
         <button id="submit" name="Submit" value="suivant" >Submit</button>
     
     </div>
+            
 
-    <script src="QuestionsTest.js"></script>
+    <!-- <script src="QuestionsTest.js"></script> -->
 
 </body>
 </html>
