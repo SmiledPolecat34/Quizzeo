50c43b6b66cb4ead81f70b9c796b679dada58238
sossoIpssi <sophie.tondeur@yahoo.com>
2023-03-14 14:26:49 +0100

    Page de score

--- score.php
+++ score.php
@@ -15,7 +15,7 @@
     </header>
     <?php
     session_start();
-    var_dump($_SESSION);
+    // var_dump($_SESSION);
     $nbrquest=$_SESSION['nbrquest'];
     $score=$_SESSION['scores'];
     function Score($nbrquest,$score){
